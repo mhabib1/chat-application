9b558263f56a9e0c4de4479cdf7f90ce73c9eb68
Mohammed Rahman <mhrrahman@Mohammeds-MacBook-Pro.local>
2022-03-15 22:34:55 -0500

    added export const in App.js

--- application/src/App.js
+++ application/src/App.js
@@ -1,2 +1,17 @@
 import {ChatEngine} from 'react-chat-engine';
 import './App.css'
+
+export const App = () => {
+    return
+    (
+        //ChatEngine is a component
+        <ChatEngine
+            //here we are going to pass all the props
+            height="100vh"// This is the view point of the application.
+            projectID="53afc2ac-927a-4c5d-87d8-32134e57fbaa
+            "// This is the string when you create your chat application.
+            userName="admin"// This is the username.
+            userSecret="mhabib123"// This is your user password.
+        />
+    );
+}
