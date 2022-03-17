f4fb75e41c7de1baa14ed5d93484045342c13801
Mohammed Rahman <mhrrahman@Mohammeds-MacBook-Pro.local>
2022-03-16 21:17:29 -0500

    fixed App.js

--- application/src/App.js
+++ application/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
             projectID="53afc2ac-927a-4c5d-87d8-32134e57fbaa
             "// This is the string when you create your chat application.
             userName="admin"// This is the username.
-            userSecret="mhabib123"// This is your user password.
+            userSecret="123456"// This is your user password.
         />
     );
 }
